53d89984db4e15fdd3a6481ed339230ba2713447
Mona Lisa <axel86065@gmail.com>
2022-10-31 12:51:36 -0600

    Monitoreo

--- vistas/src/Vistas/Barrra.js
+++ vistas/src/Vistas/Barrra.js
@@ -4,64 +4,25 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import React, { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Modal from 'react-bootstrap/Modal';
+import React, { useState } from 'react';
+
 
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
-function Monitoreo() {
+function Barra() {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const [show2, setShow2] = useState(false);
-
-    const handleClose2 = () => setShow2(false);
-    const handleShow2 = () => setShow2(true);
-
-
     return (
-        <>
+        <Container>
             <Navbar bg="dark" variant="dark">
                 <Container fluid>
-
-                    <Button variant="outline-info" onClick={handleShow2}>
-                        Options
-                    </Button>
-
-                    <Offcanvas show={show2} onHide={handleClose2}>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="publicaciones">Publicaciones</Nav.Link>
-                                <Nav.Link href="monitoreo">Monitoreo</Nav.Link>
-                                <NavDropdown
-                                    title="otros"
-                                >
-                                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action4">
-                                        Another action
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action5">
-                                        Something else here
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Buscar"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Buscar</Button>
-                            </Form>
-                        </Offcanvas.Body>
-                    </Offcanvas>
 
                     <Navbar.Brand className='ms-3' href="home">CureFish</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
@@ -116,6 +77,7 @@ function Monitoreo() {
 
                                 </NavDropdown.Item>
                             </NavDropdown>
+
                         </Nav>
                         <Form className="d-flex">
                             <Button href='login' variant="outline-info">Iniciar sesión</Button>
@@ -124,19 +86,11 @@ function Monitoreo() {
                 </Container>
             </Navbar>
 
-            <DropdownButton id="dropdown-basic-button" title="Visualizar">
-                <Dropdown.Item href="#/action-1">Temperatura</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">suciedad</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Nivel de agua</Dropdown.Item>
-            </DropdownButton>
 
 
-
-        </>
-
-
+        </Container>
     );
 
 }
 
-export default Monitoreo;
+export default Barra;
--- vistas/src/Vistas/Publicaciones.js
+++ vistas/src/Vistas/Publicaciones.js
@@ -32,7 +32,7 @@ function Publicaciones() {
                         <Offcanvas.Body>
                             <Nav className="justify-content-end flex-grow-1 pe-3">
                                 <Nav.Link href="publicaciones">Publicaciones</Nav.Link>
-                                <Nav.Link href="monitoreo">Monitoreo</Nav.Link>
+                                <Nav.Link href="monitoreoo">Monitoreo</Nav.Link>
                                 <NavDropdown
                                     title="otros"
                                 >
